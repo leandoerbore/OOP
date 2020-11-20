df1af4e7a8df34579586c03108d85fba648f4f49
leandoerbore <rusa8612331@gmail.com>
2020-11-20 12:50:43 +0300

    linq в Cleaning

--- Backup/Backup/Program.cs
+++ Backup/Backup/Program.cs
@@ -12,12 +12,23 @@ namespace Backup
             string backuppath = @"D:\Test\backups\";
             var manager = new Manager();
             manager.CreateBackup(new List<string>() {@"D:\Test\file1.txt", @"D:\Test\file2.txt"});
-            manager.CreateRestorePoint(0, backuppath);
+            manager.CreateRestorePoint(0, backuppath); // Создал full
+            manager.CreateRestorePoint(0, backuppath); // Создал full
             manager.AddFileToBackup(0, @"D:\Test\file3.txt");
-            manager.CreateDeltaRestorePoint(0, backuppath);
-            manager.CreateRestorePoint(0, backuppath);
+            manager.CreateDeltaRestorePoint(0, backuppath); // Создал delta
+            manager.CreateRestorePoint(0, backuppath); // Создал full
+            manager.CreateRestorePoint(0, backuppath); // Создал full
+            
+            
+            
+            
+            
+            /*manager.CreateRestorePoint(0, backuppath); // Создал full
+            manager.AddFileToBackup(0, @"D:\Test\file3.txt");
+            manager.CreateDeltaRestorePoint(0, backuppath); // Создал delta
+            manager.CreateRestorePoint(0, backuppath); // Создал full
             manager.AddFileToBackup(0, @"D:\Test\file4.txt");
-            manager.CreateDeltaRestorePoint(0, backuppath);
+            manager.CreateDeltaRestorePoint(0, backuppath); // Создал delta*/
             
             
             
