d62b8cd7b4092c257cdfe9e91cdb6e0df96039e2
leandoerbore <rusa8612331@gmail.com>
2020-11-22 18:29:24 +0300

    Доделал алгоритм очистки точек и добавил Exceptions.cs

--- Backup/Backup/Program.cs
+++ Backup/Backup/Program.cs
@@ -9,38 +9,25 @@ namespace Backup
     {
         static void Main(string[] args)
         {
-            var date = new DateTime(2020, 11, 19);
-            /*var datenow = DateTime.Now;
-            Console.WriteLine(date.Date);
-            Console.WriteLine(datenow);
-
-            var answer = datenow - date;
             
-            Console.WriteLine(answer.Days);*/
 
             string backuppath = @"D:\Test\backups\";
             var manager = new Manager();
-            manager.CreateBackup(new List<string>() {@"D:\Test\file1.txt", @"D:\Test\file2.txt"});
+            manager.CreateBackup(new List<string>() {@"D:\Test\file1.txt"});
             manager.CreateRestorePoint(0, backuppath); // Создал full
 
-            manager.AddFileToBackup(0, @"D:\Test\file3.txt");
-            manager.AddFileToBackup(0, @"D:\Test\file4.txt");
-            manager.AddFileToBackup(0, @"D:\Test\file5.txt");
-            manager.CreateDeltaRestorePoint(0, backuppath); // Создал delta
+            manager.AddFileToBackup(0,@"D:\Test\file3.txt" );
+            manager.CreateRestorePoint(0, backuppath); // Создал full
+
+            manager.CreateRestorePoint(0, backuppath); // Создал full
+
+            manager.CleanRestorePoints(0);
             
-            manager.SetDateToPoint(0, date); // Поставил дату, да
-            manager.SetDateToPoint(0, date); // Поставил дату, да
-            manager.CreateRestorePoint(0, backuppath); // Создал full
-
-
-
+            /*var date = new DateTime(2020, 11, 19);
+            manager.SetDateToPoint(0, date);*/
 
 
             Console.WriteLine("");
-            
-            
-            
-            
         }
     }
 }
